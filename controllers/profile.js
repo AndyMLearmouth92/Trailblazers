9a58bad1ead0cd4c9c1fc72520200a352e58c899
Andy Learmouth <andymlearmouth@hotmail.com>
2023-03-15 12:11:38 +0000

    Media queries added to the feed on the profile page and fixed the trash can.

--- controllers/profile.js
+++ controllers/profile.js
@@ -7,7 +7,8 @@ module.exports = {
   getProfile: async (req, res) => {
     try {
       const user = await User.findOne({userName : req.params.userName.toLowerCase()}).lean();
-      const posts = await Post.find({ user: user._id });
+      const posts = await Post.find({ user: user._id }).populate('user').lean();
+      console.log(posts)
       res.render("profile.ejs", { posts: posts, user: user, loggedInUser: req.user});
     } catch (err) {
       console.log(err);
@@ -32,8 +33,9 @@ module.exports = {
           $inc: { likes: 1 },
         }
       );
+      const user = await User.findById(post.user)
       console.log("Likes +1");
-      res.redirect(`/profile/${post.user}`);
+      res.redirect(`/profile/${user.userName}`);
     } catch (err) {
       console.log(err);
     }
@@ -44,10 +46,11 @@ module.exports = {
       let post = await Post.findById({ _id: req.params.id });
       await cloudinary.uploader.destroy(post.cloudinaryId);
       await Post.remove({ _id: req.params.id });
+      const user = await User.findById(post.user)
       console.log("Deleted Post");
-      res.redirect(`/profile/${req.user.id}`);
+      res.redirect(`/profile/${user.userName}`);
     } catch (err) {
-      res.redirect(`/profile/${req.user.id}`);
+      res.redirect(`/profile/${user.userName}`);
     }
   },
 };
